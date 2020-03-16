3eaa001d14d1f7b5b232e45e32b2f79ea6b709b0
Arnar <arnararnarson@gmail.com>
2020-03-16 15:25:13 +0000

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+    filename: 'bundle.js',
+  },
+  devServer: {
+    contentBase: './dist',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader', 'eslint-loader', 'style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [new webpack.optimize.UglifyJsPlugin()],
+  devtool: 'inline-source-map',
+};
