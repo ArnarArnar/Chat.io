de30d3b5ea8529d2c80c6aa664b4ff7e3cbf0614
Arnar <arnararnarson@gmail.com>
2020-03-23 13:47:36 +0000

    user name form check added

--- src/components/LogIn/LogIn.js
+++ src/components/LogIn/LogIn.js
@@ -6,14 +6,16 @@ import { addUser } from '../../Store/actions';
 //import socketService from '../../services/socketService';
 import ViewAllRooms from '../ViewAllRooms/ViewAllRooms';
 import PropTypes from 'prop-types';
-// import connectToSocketIOServer from 'socket.io-client';
-// const socket = connectToSocketIOServer('http://localhost:8080');
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+//import Spinner from 'react-bootstrap/Spinner';
 
 class LogIn extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       userName: '',
+      userNameError: null,
       isLoggedIn: false,
     };
   }
@@ -41,6 +43,23 @@ class LogIn extends React.Component {
   }
 
   onChange(e) {
+    if (e.target.name === 'userName') {
+      const userName = e.target.value;
+      if (userName.indexOf(' ') > 0) {
+        this.setState({ userNameError: 'Username cannot spaces' }, () => {
+          console.log(this.state.userNameError);
+        });
+      } else if (!userName.replace(/\s/g, '').length) {
+        this.setState(
+          { userNameError: 'Username cannot only contain whitespace' },
+          () => {
+            console.log(this.state.userNameError);
+          }
+        );
+      } else {
+        this.setState({ userNameError: null });
+      }
+    }
     this.setState({ [e.target.name]: e.target.value });
   }
 
@@ -52,44 +71,62 @@ class LogIn extends React.Component {
     let { userName } = this.state;
 
     return (
-      <div>
-        {this.state.isLoggedIn !== true ? (
-          <>
-            <Form>
-              <Form.Group controlId="formUsername">
-                <Form.Label>Create a username</Form.Label>
-                <Form.Control
-                  type="text"
-                  name="userName"
-                  value={userName}
-                  placeholder="Username"
-                  onChange={e => this.onChange(e)}
-                />
-              </Form.Group>
-              <Button
-                variant="primary"
-                type="submit"
-                onClick={e => this.logIn(e)}
-              >
-                Submit
-              </Button>
-            </Form>
-            <ViewAllRooms />
-          </>
-        ) : (
-          <>
-            <div>Logged in as: {this.props.userName}</div>
-            <Button
-              variant="primary"
-              type="submit"
-              onClick={e => this.signOut(e)}
-            >
-              Sign Out
-            </Button>{' '}
-            <ViewAllRooms />
-          </>
-        )}
-      </div>
+      <React.Fragment>
+        <Row
+          className="d-flex justify-content-center align-items-center w-100 mt-5"
+          style={{
+            minHeight: '100%',
+          }}
+        >
+          <Col>
+            {this.state.isLoggedIn !== true ? (
+              <>
+                <Form>
+                  <Form.Group controlId="formUsername">
+                    <Form.Label>Create a username</Form.Label>
+                    <Form.Control
+                      required
+                      type="text"
+                      name="userName"
+                      value={userName}
+                      placeholder="Choose a username"
+                      onChange={e => this.onChange(e)}
+                    />
+                    {this.state.userNameError !== null && (
+                      <Form.Control.Feedback
+                        style={{ display: 'block' }}
+                        type="invalid"
+                      >
+                        {this.state.userNameError}
+                      </Form.Control.Feedback>
+                    )}
+                  </Form.Group>
+                  <Button
+                    variant="primary"
+                    type="submit"
+                    onClick={e => this.logIn(e)}
+                  >
+                    Submit
+                  </Button>
+                </Form>
+                <ViewAllRooms />
+              </>
+            ) : (
+              <>
+                <div>Logged in as: {this.props.userName}</div>
+                <Button
+                  variant="primary"
+                  type="submit"
+                  onClick={e => this.signOut(e)}
+                >
+                  Sign Out
+                </Button>{' '}
+                <ViewAllRooms />
+              </>
+            )}
+          </Col>
+        </Row>
+      </React.Fragment>
     );
   }
 }
